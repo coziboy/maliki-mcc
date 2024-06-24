093a4ce55aca0e51565e0c44997385a8cc69977e
Abdullah Azzam <98200429+azzamt11@users.noreply.github.com>
2024-06-25 05:42:37 +0800

    adding footer

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -15,6 +15,9 @@ export const MainContent = () => {
       <section id="daftar">
         <Admission></Admission>
       </section>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
   );
 };
